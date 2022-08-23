28a08f689117ed7981b5889b6dfbfa6a04f98d18
Katie <103640963+CodeWordJava@users.noreply.github.com>
2022-08-23 13:42:47 +0100

    added cube to scene

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 //CAMERA
 
@@ -10,17 +11,25 @@ camera.lookAt(0,0,0)
 
 const renderer = new THREE.WebGL1Renderer( {antialias:1} )
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth,window.innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setAnimationLoop( Animate );
 document.body.appendChild( renderer.domElement )
 
 //SCENE
 const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(4,12, 0x808080))
+
+//Orbit controls
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.update()
+const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({color:0x808080}))
+scene.add(cube)
 
 export function Animate(){
 requestAnimationFrame(Animate)
 
-renderer.render(scene, camera)
+renderer.render(scene, camera,)
 }
 
 function onWindowResize(){
